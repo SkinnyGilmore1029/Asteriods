80934d0d03b59f77193efadbd4d9c31ea5d1e174
JGilmore1029 <Skinny_Gilmore@hotmail.com>
2025-03-12 08:05:49 -0500

    Making the Triangle spin fixed ordering

--- main.py
+++ main.py
@@ -11,17 +11,17 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-
+    player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
                 
         screen.fill("black")
-        player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+        dt = clock.tick(60)/1000
+        player.update(dt)
         player.draw(screen)
         
-        clock.tick(60)/1000
         pygame.display.flip()
 
 if __name__ == '__main__':
